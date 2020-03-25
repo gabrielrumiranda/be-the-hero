dbb4db0c029826edb2624c87a7a091f4275e9146
gabrielrumiranda <gabrielrumiranda@gmail.com>
2020-03-25 16:51:54 -0300

    Begin the frontend and create logon page

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './style.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function index() {
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be the Hero"/>
+          <h1>Cadastro</h1>
+          <p></p>
+        </section>
+        <form >
+
+        </form>
+      </div>
+    </div>
+  )
+}
